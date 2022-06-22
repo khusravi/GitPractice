67ae6a457a3c0a18b88aeaa439775df48736c920
Khusravi <muhammad.khusravi@tekschool.us>
2022-06-22 13:09:35 -0700

    first Exercise

--- src/day/one/PracticeClass.java
+++ src/day/one/PracticeClass.java
@@ -4,6 +4,9 @@ public class PracticeClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("First Exercise");
+		int x = 5;
+		System.out.println(x);
 
 	}
 
